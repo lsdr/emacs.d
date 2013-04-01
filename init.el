5ca95b34901fd0eb543ac139dfaeee1e0f06dabd
Luiz Rocha <lsdrocha@gmail.com>
2013-04-01 17:28:44 -0300

    list of packages to install if not present

--- init.el
+++ init.el
@@ -9,3 +9,15 @@
 ;;  '(tool-bar-mode nil))
 (set-default-font "Monaco 12")
 (custom-set-variables '(tool-bar-mode nil))
+
+;; list of packages which will be installed if they are not be present
+;; from: https://github.com/technomancy/emacs-starter-kit
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js color-theme-solarized monokai-theme)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
