03080dfb28cfcf999d793ef986f6b79a3d727ed9
Luiz Rocha <lsdrocha@gmail.com>
2013-05-01 21:21:03 -0300

    load-theme monokai

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 ;; (custom-set-variables
 ;;  '(tool-bar-mode nil))
 (set-default-font "Monaco 12")
-(custom-set-variables '(tool-bar-mode nil))
+(load-theme 'monokai)
 
 ;; list of packages which will be installed if they are not be present
 ;; from: https://github.com/technomancy/emacs-starter-kit
@@ -24,3 +24,19 @@
 
 ;; setting up backup
 (setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
+
+;; let Custom have it its way...
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("d24e10524bb50385f7631400950ba488fa45560afcadd21e6e03c2f5d0fad194" default)))
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
