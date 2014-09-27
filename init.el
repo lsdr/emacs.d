9efe19262bfd247953c374cdb3397056dc8555bd
Luiz Rocha <lsdrocha@gmail.com>
2014-09-27 01:29:24 -0300

    removing monokai theme

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (custom-set-variables
@@ -8,7 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("d24e10524bb50385f7631400950ba488fa45560afcadd21e6e03c2f5d0fad194" default))))
+ '(custom-safe-themes (quote ("9a9e75c15d4017c81a2fe7f83af304ff52acfadd7dde3cb57595919ef2e8d736" "d24e10524bb50385f7631400950ba488fa45560afcadd21e6e03c2f5d0fad194" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,24 +18,23 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; user interface customizations
-;; (set-face-attribute 'default nil :font "Monaco 12")
-;; (custom-set-variables
-;;  '(tool-bar-mode nil))
-(set-default-font "Droid Sans Mono 14")
-(load-theme 'monokai)
-
 ;; list of packages which will be installed if they are not be present
 ;; from: https://github.com/technomancy/emacs-starter-kit
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js monokai-theme)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;; user interface customizations
+;; (set-face-attribute 'default nil :font "Monaco 12")
+;; (custom-set-variables
+;;  '(tool-bar-mode nil))
+(set-default-font "Droid Sans Mono 14")
 
 ;; setting up backup
 (setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
